c5ba327bd451b5ba70c9a8318d8e78f28edce6fd
Revealll <116298146+Revealll@users.noreply.github.com>
2023-01-19 18:13:40 -0500

    Update main.js

--- ANDYZHUA4/scripts/main.js
+++ ANDYZHUA4/scripts/main.js
@@ -3,7 +3,10 @@ const I_ADDA = document.getElementById("addArtist");
 const I_ADDL = document.getElementById("addLink");
 const C_FAV = document.getElementById("favourite");
 
-
+let songName = ['Baby']
+let songArtist = ['Justin Bieber']
+let link = ['https://www.youtube.com/watch?v=kffacxfA7G4']
+let favourite = [true]
 function showPlayList() {
 
 }
