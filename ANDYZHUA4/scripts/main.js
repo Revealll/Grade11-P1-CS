464e5181ee4cfebf7b54f5f664fc1660af83b791
Revealll <116298146+Revealll@users.noreply.github.com>
2023-01-19 18:09:42 -0500

    qwe

---
+++ ANDYZHUA4/scripts/main.js
@@ -0,0 +1,9 @@
+const I_ADDS = document.getElementById("addSong");
+const I_ADDA = document.getElementById("addArtist");
+const I_ADDL = document.getElementById("addLink");
+const C_FAV = document.getElementById("favourite");
+
+
+function showPlayList() {
+
+}
